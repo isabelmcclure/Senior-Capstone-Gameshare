0ae1796a112424b3072c5d31180bd71ccad2cf33
stanimals21 <shawntan2008@gmail.com>
2021-03-14 17:29:29 -0500

    added initial setup for dashboard/landing page

---
+++ pages/dashboard.js
@@ -0,0 +1,10 @@
+const Dashboard = () => {
+    return ( 
+        <div className="dashboard">
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis consequuntur numquam quia rem, nostrum error autem, et eius suscipit eaque quae! Reprehenderit fugit vero ipsa, accusantium reiciendis hic incidunt sit.</h1>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis consequuntur numquam quia rem, nostrum error autem, et eius suscipit eaque quae! Reprehenderit fugit vero ipsa, accusantium reiciendis hic incidunt sit.</h1>
+        </div>
+     );
+}
+ 
+export default Dashboard;
