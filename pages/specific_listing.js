8ece2593a33ed65766d6df18ee0ec32ba7acbe4d
keerat23 <keerat23@tamu.edu>
2021-03-23 18:01:47 -0500

    variables added that will be displayed on page

--- pages/specific_listing.js
+++ pages/specific_listing.js
@@ -1,8 +1,17 @@
+import React, { useState } from 'react';
+
 export default function SpecificListing() {
+
+  const [name, setName] = useState("Example BoardGame")
+  const [quality, setQuality] = useState(5)
+  const [description, setDescription] = useState("This is an example board game that will be displayed in the specifc listing page on GameShare")
+  const [imageUrl, setImageUrl] = useState("image_url")
+
   return (
 
     <div>
       <main>
+        {name}
       </main>
     </div>
   )
