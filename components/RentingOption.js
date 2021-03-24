2db83d811db67d3a0f78da147171c8993e17e817
keerat23 <keerat23@tamu.edu>
2021-03-23 21:49:00 -0500

    added buttons for report, contact and rent

--- components/RentingOption.js
+++ components/RentingOption.js
@@ -1,9 +1,12 @@
+import { PromiseProvider } from 'mongoose';
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button'
 //import boardgameImage from '.public/boardgame.jpeg'
 import DatePicker from 'react-datepicker'
 import style from '../styles/RentingOption.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function RentingOption() {
+export default function RentingOption(props) {
 
   const [startDate, setStartDate] = useState(new Date())
   const [endDate, setEndDate] = useState(new Date())
@@ -14,7 +17,7 @@ export default function RentingOption() {
   const endDateSelected = (value, event) => {
     if(!(startDate > value)){
       setEndDate(value)
-      setRentAmount(Math.floor((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)))
+      setRentAmount(Math.floor((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)) * props.rate)
     }
   }
 
@@ -27,10 +30,15 @@ export default function RentingOption() {
         Select rent date: <DatePicker className={style.card} selected={startDate} onChange={startDateSelected}/>
       </div>
       <div className={style.rowContainer}>
-        Select rent date: <DatePicker className={style.card} selected={endDate} onChange={endDateSelected}/>
+        Select return date: <DatePicker className={style.card} selected={endDate} onChange={endDateSelected}/>
       </div>
       <img className={style.image} src='/location.png'/>
-      {rentAmount}
+      <h1 className={style.description}>Amount to rent: ${rentAmount}</h1>
+      <div clasName={style.rowContainer}>
+        <Button className={style.btn} variant="danger">Report</Button>
+        <Button className={style.btn} variant="secondary">Contact Lender</Button>
+        <Button className={style.btn}>Rent</Button>
+      </div>
     </div>
   )
 }
