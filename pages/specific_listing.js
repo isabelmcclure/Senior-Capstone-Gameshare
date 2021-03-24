bbf5bace06bac7f786ba5f14fa6774706a91931a
keerat23 <keerat23@tamu.edu>
2021-03-23 19:02:50 -0500

    basic listing display

--- pages/specific_listing.js
+++ pages/specific_listing.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
+//import boardgameImage from '.public/boardgame.jpeg'
+import style from '../styles/SpecificListing.module.css'
 
 export default function SpecificListing() {
 
   const [name, setName] = useState("Example BoardGame")
   const [quality, setQuality] = useState(5)
   const [description, setDescription] = useState("This is an example board game that will be displayed in the specifc listing page on GameShare")
-  const [imageUrl, setImageUrl] = useState("image_url")
+  const [imageUrl, setImageUrl] = useState("/boardgame.jpeg")
 
   return (
 
-    <div>
+    <div className={style.container}>
       <main>
-        {name}
+          <h1 className={style.title}>{name}</h1>
+          <img src={imageUrl} alt="Vercel Logo"/>
+          <h1 className={style.description}>What is {name}?</h1>
+          {description}
+          <h1 className={style.description}>Quailty: {quality}</h1>
       </main>
     </div>
   )
