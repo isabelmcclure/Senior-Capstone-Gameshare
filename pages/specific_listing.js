f1a1389bb9660fe74051ad0ad27852445d1f87d5
keerat23 <keerat23@tamu.edu>
2021-03-23 22:14:34 -0500

    added a navbar that does nothing, just for looks for now

--- pages/specific_listing.js
+++ pages/specific_listing.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 //import boardgameImage from '.public/boardgame.jpeg'
+import Navbar from '../components/Navbar.js'
 import Listing from '../components/Listing.js'
 import RentingOption from '../components/RentingOption.js'
 import 'react-datepicker/dist/react-datepicker.css'
@@ -9,9 +10,12 @@ export default function SpecificListing() {
 
   return (
     
-    <div className={style.container}>
-      <Listing name="Example Board Game" quality="5" description="This is an example board game description" imageUrl="/boardgame.jpeg" rate="$5/day"></Listing>
-      <RentingOption rate={5}/>
+    <div className={style.columnContainer}>
+      <Navbar></Navbar>
+      <div className={style.rowContainer}>
+        <Listing name="Example Board Game" quality="5" description="This is an example board game description" imageUrl="/boardgame.jpeg" rate="$5/day"></Listing>
+        <RentingOption rate={5}/>
+      </div>
     </div>
   )
 }
