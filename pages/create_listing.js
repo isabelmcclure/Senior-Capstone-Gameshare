8faa0c0a98817f76bfe8384265af828397290f8f
keerat23 <keerat23@tamu.edu>
2021-03-30 12:58:19 -0500

    Initial tailwind styling

--- pages/create_listing.js
+++ pages/create_listing.js
@@ -3,46 +3,46 @@ import React, { useState } from 'react';
 
 export default function SpecificListing() {
 
-  const description = "This is an example board game description. Everything beyond this sentence will be nonsense to fill in the page. Board games have a long tradition in Europe. The oldest records of board gaming in Europe date back to Homer's Iliad (written in the 8th century BC), in which he mentions the Ancient Greek game of Petteia.[15] This game of petteia would later evolve into the Roman Ludus Latrunculorum.[15] Board gaming in ancient Europe was not unique to the Greco-Roman world, with records estimating that the ancient Norse game of Hnefatafl was developed sometime before 400AD.[16] In ancient Ireland, the game of Fidchell or Ficheall, is said to date back to at least 144 AD,[17] though this is likely an anachronism. A fidchell board dating from the 10th century has been uncovered in Co. Westmeath, Ireland.[18]"
-
   return (
     
     <div>
       <h1>Create a Listing</h1>
-      <form action="/" method="post">
-        <div>
-          <label for="game_name">Name of Game</label>
-          <input type="text" name="game_name" id="game_name"></input>
+      <form className="mb-6" action="/" method="post">
+        <div className="flex flex-col mb-4 ">
+          <label className="mb-2 uppercase font-bold text-lg text-grey-darkest" for="game_name">Name of Game</label>
+          <input className="border py-2 px-3 text-grey-darkest" type="text" name="game_name" id="game_name"></input>
         </div>
-        <div>
-          <label for="description">Description</label>
-          <input type="text" name="description" id="description"></input>
+        <div className="flex flex-col mb-4">
+          <label className="mb-2 uppercase font-bold text-lg text-grey-darkest" for="description">Description</label>
+          <input className="border py-2 px-3 text-grey-darkest" type="text" name="description" id="description"></input>
         </div>
-        <div>
-          <label for="quality">Quality</label>
-          <input type="text" name="quality" id="quality"></input>
+        <div className="flex flex-col mb-4">
+          <label className="mb-2 uppercase font-bold text-lg text-grey-darkest" for="quality">Quality</label>
+          <input className="border py-2 px-3 text-grey-darkest" type="text" name="quality" id="quality"></input>
         </div>
-        <div>
-          <label for="pictures">Pictures</label>
-          <input type="text" name="pictures" id="pictures"></input>
+        <div className="flex flex-col mb-4">
+          <label className="mb-2 uppercase font-bold text-lg text-grey-darkest" for="pictures">Pictures</label>
+          <input className="border py-2 px-3 text-grey-darkest" type="text" name="pictures" id="pictures"></input>
         </div>
-        <div>
-          <label for="price">Price</label>
-          <input type="text" name="price" id="price"></input>
+        <div className="flex flex-col mb-4">
+          <label className="mb-2 uppercase font-bold text-lg text-grey-darkest" for="price">Price</label>
+          <input className="border py-2 px-3 text-grey-darkest" type="text" name="price" id="price"></input>
         </div>
-        <div>
-          <label for="start_date">Start Date</label>
-          <input type="date" name="start_date" id="start_date"></input>
+        <div className="flex flex-col mb-4">
+          <label className="mb-2 uppercase font-bold text-lg text-grey-darkest" for="start_date">Start Date</label>
+          <input className="border py-2 px-3 text-grey-darkest" type="date" name="start_date" id="start_date"></input>
         </div>
-        <div>
-          <label for="end_date">End Date</label>
-          <input type="date" name="end_date" id="end_date"></input>
+        <div className="flex flex-col mb-4">
+          <label className="mb-2 uppercase font-bold text-lg text-grey-darkest" for="end_date">End Date</label>
+          <input className="border py-2 px-3 text-grey-darkest" type="date" name="end_date" id="end_date"></input>
         </div>
-        <div>
-          <label for="location">Location</label>
-          <input type="text" name="location" id="location"></input>
+        <div className="flex flex-col mb-4">
+          <label className="mb-2 uppercase font-bold text-lg text-grey-darkest" for="location">Location</label>
+          <input className="border py-2 px-3 text-grey-darkest" type="text" name="location" id="location"></input>
         </div>
-        <button type="submit">Create Listing</button>
+        <div className="flex flex-col mb-4">
+          <button className="block bg-green-500 hover:bg-green-600 text-white uppercase text-lg mx-auto p-4 rounded">Create Listing</button>
+        </div>
       </form>
     </div>
   )
