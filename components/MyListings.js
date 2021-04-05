13be65de4e2590489f7bf9fbbd8c8c931943786f
keerat23 <keerat23@tamu.edu>
2021-04-05 12:40:24 -0500

    redirect to create listing page using router

--- components/MyListings.js
+++ components/MyListings.js
@@ -1,4 +1,9 @@
+import { useRouter } from 'next/router';
+
 const MyListings = () => {
+
+    const router = useRouter();
+
     return (
         <div>
             <h1 className="mt-7 w-5/6 mx-auto text-3xl">My Listings</h1>
@@ -38,7 +43,7 @@ const MyListings = () => {
                 </tr> 
             </table>
             <div className="pt-5 p-28">
-                <button className="font-mono bg-blue-500 p-2 mr-2 rounded text-green-200">Add New Listing</button>
+                <button className="font-mono bg-blue-500 p-2 mr-2 rounded text-green-200" onClick={() => {router.push('/create_listing')}}>Add New Listing</button>
             </div>
         </div>
     );
