dfc39b5f3345339a282bab8820e998d2686fd24e
stanimals21 <shawntan2008@gmail.com>
2021-04-14 01:07:33 -0500

    refactored listing page components and fixed bugs with searching

--- components/QueryListings.js
+++ components/QueryListings.js
@@ -1,46 +1,49 @@
 import React, {Component, useState } from 'react';
+import QueryBar from "../components/QueryBar";
 
 const QueryListing = (props) => {
 
     const [title, setTitle] = useState("");
-    const [bg_filtered, setBG] = useState(props.listings);
+    const [max_price, setMaxPrice] = useState("");
+    const listings = props.listings;
+    const [bg_filtered, setBG] = useState(listings);
     
     const handleGameNameQuery = (e) => {
         setTitle(e.target.value);
+    }
+
+    const handlePriceQuery = (e) => {
+        setMaxPrice(e.target.value);
     }
     
     const handleSubmit = (e) => {
         // prevent form from refreshing the page
         e.preventDefault();
 
-        setBG(bg_filtered.filter((bg) => bg.title === title));
+        // reset bg_filtered state variable
+        setBG(listings);
+
+        if(title !== ""){
+            const temp = bg_filtered.filter((bg) => bg.title === title);
+            setBG(temp);
+        }
+        console.log(bg_filtered);
+        
+        if(max_price != ""){
+            setBG(bg_filtered.filter((bg) => bg.price <= max_price));
+        }
+        console.log(bg_filtered);
+
     }
 
     return (
         <div>
             {/* <QueryBar> */}
-            <div className= "max-w-3xl mx-auto pt-4">
-                <form className="flex justify-center bg-blue-300 p-4 rounded-full shadow-lg" onSubmit={handleSubmit}>
-                    <label className="p-3">
-                        <div className = "font-mono font-extrabold text-blue-700 text-xl text-center">Name:</div>
-                        <input className= "rounded-full p-1" type="text" name="game_name" id="game_name" placeholder="Monopoly" onChange={handleGameNameQuery}></input>
-                    </label>
+            <QueryBar handleSubmit={handleSubmit} 
+                    handleGameNameQuery={handleGameNameQuery} 
+                    handlePriceQuery={handlePriceQuery}>
+            </QueryBar>
 
-                    <label className="p-3">
-                        <div className="font-mono font-extrabold text-blue-700 text-xl text-center">Location:</div>
-                        <input className= "rounded-full p-1" type="text" name="location" id="location" placeholder="College Station"></input>
-                    </label>
-
-                    <label className="p-3">
-                        <div className="font-mono font-extrabold text-blue-700 text-xl text-center">Duration:</div>
-                        <input className= "rounded-full p-1" type="date" name="date"></input>
-                    </label>
-
-                    <input className="rounded-full ml-6 py-3 px-6" type="submit" value="Submit"/>
-                </form>
-            </div>
-
-            {/* QueryListings */}
             <h1 className="mt-7 w-5/6 mx-auto text-3xl">Current Listings</h1>
             <table className="rounded-t-lg m-5 w-5/6 mx-auto bg-gray-200 text-gray-800">
                 <tbody>
@@ -58,7 +61,9 @@ const QueryListing = (props) => {
                                 <td className="px-4 py-3">${boardgame.price}</td>
                                 <td className="px-4 py-3">{boardgame.postedAt}</td>
                                 <td className="px-4 py-3">
-                                    <button className="bg-blue-500 p-2 rounded text-green-200">View</button>
+                                    <button className="bg-blue-500 p-2 rounded text-green-200 pl-2">View</button>
+                                </td>
+                                <td className="px-4 py-3">
                                     <button className="bg-blue-500 p-2 rounded text-green-200">Edit</button>
                                 </td>
                             </tr>
