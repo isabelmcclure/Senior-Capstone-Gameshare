15435302499f2b73a053b9287aa202b00cdc2401
isabelmcclure <isabelmcclure@tamu.edu>
2021-04-19 12:06:11 -0500

    addition to prior commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,12 +23,7 @@ const Navbar = () => {
 
                     {/* secondary nav */}
                     <div className="flex justify-between space-x-1">
-<<<<<<< HEAD
                         <Link href="/login"><a className="px-3 py-5">Login/Sign Up</a></Link>
-=======
-                        <Link href="/login"><a className="px-3 py-5">Login</a></Link>
-                        <Link href="/signUp"><a className="px-3 py-5">Sign Up</a></Link>
->>>>>>> main
                     </div>
 
                 </div>
--- pages/login.js
+++ pages/login.js
@@ -1,36 +1,11 @@
-<<<<<<< HEAD
-import { useRouter } from 'next/router'
-import { Magic } from 'magic-sdk'
-import styles from '../styles/Home.module.css'
-import Head from 'next/head'
-=======
 import LogInSignUp from '../components/LogInSignUp.js'
->>>>>>> main
 
 export default function Login() {
   
 
     return (
-<<<<<<< HEAD
-        <div className= {styles.login}>
-            <Head>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat"/>
-                <title>GameShare</title>
-                <link rel="icon" href="/GS.png" />
-            </Head>
-            
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="email">Email</label>
-                    <input name="email" type="email" />
-                    <button class="bg-transparent hover:bg-black-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-white-500 hover:border-white-500 rounded">
-                    Log in
-                    </button>
-                </form>
-        
-=======
         <div>
             <LogInSignUp type="Log In"></LogInSignUp>
->>>>>>> main
         </div>
     )
 }
