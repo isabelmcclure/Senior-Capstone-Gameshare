9c0d2e4dcc47312c96a4e0bd602188019cd366ec
Eric Ma <me982@tamu.edu>
2021-04-21 20:06:19 -0500

    testing for useEffect

--- pages/success.js
+++ pages/success.js
@@ -29,7 +29,7 @@ export default function Success({ props }) {
     }
 
     useEffect(() => {
-        if (customerName == "" || sellerEmail == "") {
+        while (customerName == "" && sellerEmail == "") {
             getInfo()
         }
     })
