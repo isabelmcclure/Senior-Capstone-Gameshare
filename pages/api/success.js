fda70ac2c03ae962a8a9558fbbad7a4c5083099e
Eric Ma <me982@tamu.edu>
2021-04-21 19:50:22 -0500

    test

--- pages/api/success.js
+++
@@ -1,8 +0,0 @@
-const stripe = require('stripe')('sk_test_51IdoMPBL7EVXf27yRov1hIaRrRPbaUi0lS7YjYduTZLWUpw9u3rTFut2GB7Yu2QPHcRHu0t6RtYdKS691QcFpLVt00Fs1ArB3T');
-
-export default async (req, res) => {
-    const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
-    const customer = await stripe.customers.retrieve(session.customer);
-
-    res.send(`<html><body><h1>Thanks for your order, ${customer.name}!</h1></body></html>`);
-}
